826c4aed13d27a5f9c88ad053545a14ecf3e394f
simodevjs <simodevjs@gmail.com>
2023-11-13 16:38:13 +0100

    222

--- main.c
+++ main.c
@@ -1,1 +1,22 @@
-eifieji
+/**
+ * main - Tthis is my imple shell main.c
+ * @argc: The number of arguments passed to my program
+ * @argv: An array[string] of the arguments passed to my program
+ * Return: The status code of the last execution
+ */
+
+int main(int argc, char **argv)
+{
+	char *lone = NULL;
+	char **comodo = NULL;
+	int stato;
+
+
+	while (1)
+	{
+		lone = real_lone();
+		comodo = tokonzo(lone);
+		stato = exo(comdo, argv)
+	}
+	return 0
+}
