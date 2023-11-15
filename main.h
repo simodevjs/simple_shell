7907d8d05f6cc799f4d0b9c4af5aec8bb38790c9
simodevjs <simodevjs@gmail.com>
2023-11-15 03:12:23 +0100

    222

--- main.h
+++ main.h
@@ -16,5 +16,12 @@
 
 
 char *real_lone(void);
+int _strlen(char *str);
+char *_strdup(char *str);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+
+
 
 #endif /* MAIN_H */
